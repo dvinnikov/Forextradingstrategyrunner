747c3cbdf1d9ad7899f2849d83921e31458be7ed
dvinnikov <samui2006@ukr.net>
2025-10-22 19:49:10 +0300

    Remove tailwind-merge dependency

--- src/components/ui/utils.ts
+++ src/components/ui/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+
+function mergeTailwindClasses(classNames: string): string {
+  const uniqueClasses: string[] = [];
+  const seen = new Set<string>();
+
+  for (const className of classNames.split(/\s+/)) {
+    if (!className) continue;
+    if (!seen.has(className)) {
+      seen.add(className);
+      uniqueClasses.push(className);
+    }
+  }
+
+  return uniqueClasses.join(" ");
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return mergeTailwindClasses(clsx(inputs));
 }
